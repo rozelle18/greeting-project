246d1348fad4e789dfc455bb63ecd3569ca6d198
Zor <ubuntu.root3768@gmail.com>
2018-08-07 20:56:54 +0800

    debugged the asteroid

--- js/main-demo.js
+++ js/main-demo.js
@@ -4,16 +4,38 @@ var nightsky_cg = new PIXI.Application(w, h, { antialias: true});
 
 document.getElementById('meteor-container').appendChild(nightsky_cg.view);
 
-var numberOfStars = 150;
-var starRadius = 2;
+var numberOfStars = 100;
+var starRadius = 8;
 
 for (let i = 0; i < numberOfStars; i++){
     var star = new PIXI.Graphics();
     star.beginFill(0xFFFFFF, Math.random().toFixed(2));
-    star.drawCircle(randomizer(w), randomizer(h), starRadius);
+    star.drawCircle(0, 0, starRadius);
     star.endFill();
+    star.position.x = w/2;
+    star.position.y = h/2;
     nightsky_cg.stage.addChild(star);
 }
+
+var z = w;
+nightsky_cg.ticker.speed = 1;
+nightsky_cg.ticker.add(function(delta) {
+    z = z - delta;
+    nightsky_cg.stage.children.forEach(function(star){
+        //console.log(star);
+        var sx = map_range(star.position.x/z, 0, 1, 0, w);
+        var sy = map_range(star.position.y/z, 0, 1, 0, w);
+        star.position.set(sx,sy);
+        // star.moveTo(500, 500);
+        //star.position.y = map_range(star.position.y/z, 0, 1, 0, h);
+        //star.position.x = map_range(star.position.x/z, 0, 1, 0, w);
+        // console.log(star.position.y);
+        //console.log(map_range(star.position.y/z, 0, 1, 0, h));
+        // console.log('x' + map_range(star.position.x/z, 0, 1, 0, w));
+        // console.log('y' + map_range(star.position.y/z, 0, 1, 0, h));
+    })
+});
+
 
 
 // var circle = new PIXI.Graphics();
@@ -33,8 +55,13 @@ for (let i = 0; i < numberOfStars; i++){
 //     rect.y += 1 * delta;
 // });
 
-var cont = new ScrollMagic.Controller({vertical: true});
-
 function randomizer(end){
     return Math.floor((Math.random() * end) + 1);
+}
+
+function map(value, minA, maxA, minB, maxB) {
+    return (1 - ((value - minA) / (maxA - minA))) * minB + ((value - minA) / (maxA - minA)) * maxB;
+}
+function map_range(value, low1, high1, low2, high2) {
+    return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
 }
