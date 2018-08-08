b8b37284fd5f8545dc535928b794824f575b0530
rozelle18 <winzor@simplexi.com.ph>
2018-08-08 15:02:38 +0800

    refactored code into something more understandable for our IQ

--- js/main-demo.js
+++ js/main-demo.js
@@ -1,77 +1,4 @@
-//
-//
-// var h = window.innerHeight;
-// var w = window.innerWidth;
-// var nightsky_cg = new PIXI.Application(w, h, { antialias: true});
-// document.getElementById('meteor-container').appendChild(nightsky_cg.view);
-// var numberOfStars = 400;
-// var starRadius = 8;
-//
-// for (let i = 0; i < numberOfStars; i++){
-//     var star = new PIXI.Graphics();
-//     star.beginFill(0xFFFFFF, Math.random().toFixed(2));
-//     star.drawCircle(0, 0, starRadius);
-//     star.endFill();
-//     star.position.x = getRandomInt(-w, w);
-//     star.position.y = getRandomInt(-h, h);
-//     star.z = getRandomInt(1, w);
-//     nightsky_cg.stage.addChild(star);
-// }
-//
-// nightsky_cg.ticker.speed = .01;
-// nightsky_cg.ticker.add(function(delta) {
-//     nightsky_cg.stage.children.forEach(function(star){
-//         //console.log(star)
-//         star.z -= 1;
-//         if (star.z < 1) {
-//             star.z = w;
-//             star.position.x = getRandomInt(-w, w);
-//             star.position.y = getRandomInt(-h, h);
-//         }
-//         var sx = map_range(star.position.x / star.z, 0, 1, 0, w);
-//         var sy = map_range(star.position.y / star.z, 0, 1, 0, w);
-//         star.position.set(sx, sy);
-//         // star.moveTo(500, 500);
-//         //star.position.y = map_range(star.position.y/z, 0, 1, 0, h);
-//         //star.position.x = map_range(star.position.x/z, 0, 1, 0, w);
-//         // console.log(star.position.y);
-//         //console.log(map_range(star.position.y/z, 0, 1, 0, h));
-//         // console.log('x' + map_range(star.position.x/z, 0, 1, 0, w));
-//         // console.log('y' + map_range(star.position.y/z, 0, 1, 0, h));
-//     })
-// });
-//
-//
-//
-// // var circle = new PIXI.Graphics();
-// // // set a fill and line style
-// // circle.beginFill(0xFFFFFF, .5);
-// // circle.lineStyle(4, 0xffd900, 1);
-// // circle.drawCircle(470, 90,60);
-// // circle.endFill();
-// // nightsky_cg.stage.addChild(circle);
-// // app.ticker.add(function(delta) {
-// //     var speed = 0.5;
-// //     console.log(circle.x < (screen.width/3));
-// //     if(circle.x < (screen.width/3)){
-// //         speed = 2;
-// //     }
-// //     circle.x += speed * delta;
-// //     rect.y += 1 * delta;
-// // });
-//
-// // function randomizer(end){
-// //     return Math.floor((Math.random() * end) + 1);
-// // }
-//
-// function getRandomInt(min, max) {
-//     return Math.floor(Math.random() * (max - min + 1)) + min;
-// }
-// function map_range(value, low1, high1, low2, high2) {
-//     return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
-// }
 
-//--------------------------
 window.addEventListener('load', setup);
 let width = window.innerWidth;
 let height = window.innerHeight;
@@ -79,31 +6,29 @@ let height = window.innerHeight;
 // let height = 256
 
 var params = {
-    speed: 10,
-}
+    speed: 15,
+    stars: 250
+};
 let app = new PIXI.Application({
     width: width,
     height: height,
     antialias: true
-})
-
+});
 let stars = [];
-
 function setup() {
     document.getElementById('sketch-holder').appendChild(app.view);
-
     let cont = new PIXI.Container();
     app.stage.addChild(cont);
     cont.x = width / 2;
     cont.y = height / 2;
-    for (let i = 0; i < 150; i++) {
+    for (let i = 0; i < params.stars; i++) {
         stars[i] = new Star();
         cont.addChild(stars[i].graphic);
     }
     let drawStuff = () => {
-        stars.forEach(s => {
-            s.update();
-            s.show();
+        stars.forEach(star => {
+            star.update();
+            star.show();
         })
     };
     app.ticker.add(drawStuff);
@@ -140,32 +65,25 @@ class Star {
             this.x = random(-width, width);
             this.y = random(-height, height);
             this.z = random(1, width);
+            //previous z
             this.pz = this.z;
         }
     }
 
     show() {
-
-        this.graphic.clear()
-
+        this.graphic.clear();
         const sx = map(this.x / this.z, 0, 1, 0, width);
         const sy = map(this.y / this.z, 0, 1, 0, height);
-
-
         const r = map(this.z, 0, width, 16, 0);
-
         this.graphic.lineStyle(0);
         this.graphic.beginFill(0xFFFFFF);
         this.graphic.drawCircle(sx, sy, r / 4);
         this.graphic.endFill();
-
         const px = map(this.x / this.pz, 0, 1, 0, width);
         const py = map(this.y / this.pz, 0, 1, 0, height);
-
-        this.graphic.lineStyle(4, 0xffffff, 1)
-        this.graphic.moveTo(px, py)
-        this.graphic.lineTo(sx, sy)
-
+        this.graphic.lineStyle(4, 0xffff99, 1);
+        this.graphic.moveTo(px, py);
+        this.graphic.lineTo(sx, sy);
         this.pz = this.z;
     }
 }
