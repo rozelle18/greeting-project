80034b6dae777c839222de3a4f9b9453f0aad680
rozelle18 <winzor@simplexi.com.ph>
2018-08-08 11:10:31 +0800

    initial commit before pull from master

--- js/main-demo.js
+++ js/main-demo.js
@@ -5,16 +5,32 @@ var nightsky_cg = new PIXI.Application(w, h, { antialias: true});
 document.getElementById('meteor-container').appendChild(nightsky_cg.view);
 
 var numberOfStars = 150;
-var starRadius = 2;
-
+var starRadius = 8;
 for (let i = 0; i < numberOfStars; i++){
     var star = new PIXI.Graphics();
     star.beginFill(0xFFFFFF, Math.random().toFixed(2));
-    star.drawCircle(randomizer(w), randomizer(h), starRadius);
+    star.drawCircle(0, 0, starRadius);
     star.endFill();
+    star.x = randomizer(w);
+    star.y = randomizer(h);
     nightsky_cg.stage.addChild(star);
 }
+var z = w;
+nightsky_cg.ticker.add(function(delta) {
+    z -= 1;
+    //console.log(parseInt(nightsky_cg.stage.getChildAt(0).x)/z);
+    //console.log(nightsky_cg.stage.getChildAt(0).position);
+    //nightsky_cg.stage.getChildAt(0).setTransform(z,z);
+    nightsky_cg.stage.children.forEach(function(star){
+        star.x += map(star.x / z , 0, 1, 0, w) ;
+        star.y += map(star.y / z, 0, 1, 0, h);
+    });
+    //console.log(z);
+});
 
+function map(value, minA, maxA, minB, maxB) {
+    return (1 - ((value - minA) / (maxA - minA))) * minB + ((value - minA) / (maxA - minA)) * maxB;
+}
 
 // var circle = new PIXI.Graphics();
 // // set a fill and line style
